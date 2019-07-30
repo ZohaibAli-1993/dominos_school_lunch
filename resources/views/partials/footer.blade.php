252b0de2e204fe42768433c8c2f541c8ae3345b4
Maryna <marynahaidashevska@gmail.com>
2019-07-30 17:49:43 -0500

    footer updated

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -12,21 +12,21 @@
 				      We choose our ingredients on the basis of safety, taste, and nutritional<br />
 				      content to bring our consumers what they want. Dominos Pizza dedicates<br />
 				      its attention, energy, and resourses to one mission: deliver a delicious,<br />
-				      not and fresh pizza every time.
+				      hot and fresh pizza every time.
 				      </p>
 				  	</div><!-- footer-para ends -->
 				  </div><!-- col ends -->
 				  	<div class="row">
 			            <div class="col-auto">
-			            	<span class="footer-links"><a href="#">About us</a></span><br />
-			            	<span class="footer-links"><a href="#">Privacy</a></span><br />
-			            	<span class="footer-links"><a href="#">Term of use</a></span><br />
+			            	<span class="footer-links"><a href="#" target="_blank">About us</a></span><br />
+			            	<span class="footer-links"><a href="#" target="_blank">Privacy</a></span><br />
+			            	<span class="footer-links"><a href="#" target="_blank">Term of use</a></span><br />
 			            </div><!-- col ends -->
 			        
 			            <div class="col-auto">
-			            	<span class="footer-links"><a href="#">Contact us</a></span><br />
-			            	<span class="footer-links"><a href="#">Nutrition guide</a></span><br />
-			            	<span class="footer-links"><a href="#">Gift cards</a></span><br />
+			            	<span class="footer-links"><a href="#" target="_blank">Contact us</a></span><br />
+			            	<span class="footer-links"><a href="#" target="_blank">Nutrition guide</a></span><br />
+			            	<span class="footer-links"><a href="#" target="_blank">Gift cards</a></span><br />
 			            </div><!-- col ends -->
 		            </div><!-- row ends -->
 		        </div><!-- row ends -->
@@ -46,13 +46,16 @@
                         <div class="col-sm-8"> 	
 					  	   	<img src="img/logo_footer.png" style="margin-left: 170px; width: 30%;" >
 	
-					  	   	<a href="https://www.facebook.com/dominoswinnipeg/"><img src="img/fb.png" style="margin-left: 70px; padding-left: 30px;" ></a>
-					  	   	<img src="img/twitter.png" style="padding-left: 30px;" >
-					  	   	<img src="img/instagram.png" style="padding-left: 30px;" >
+					  	   	<a href="https://www.facebook.com/dominoswinnipeg/" target="_blank">
+					  	   		<img src="img/fb.png" 
+					  	   		alt="facebook icon" style="margin-left: 70px; padding-left: 30px;" ></a>
+					  	   	<img src="img/twitter.png" alt="twitter icon" style="padding-left: 30px;" >
+					  	   	<img src="img/instagram.png" alt="instagram icon" style="padding-left: 30px;" >
+					  	</div>
 					  	   	
-					  	   </div>
+					</div>
 				  	        
-	                    </div><!-- row ends -->
+	    </div><!-- row ends -->
 
            
                		   
