d7f4306315136049c774945772f9c0f05f8957c1
root <root@localhost.localdomain>
2019-07-30 15:55:15 -0500

    Success page updated"

--- resources/views/main/sucess.blade.php
+++ resources/views/main/sucess.blade.php
@@ -4,5 +4,7 @@
 
 <div class="success icon .text-danger">
     <i class="far fa-check-circle"></i>
+    <h3>Thanks for your message!</h3>
+    <h4>We will be in touch soon.</h4>
 </div>
 @endsection
