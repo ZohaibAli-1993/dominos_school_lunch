282071e44a9c7318ea600f2588c4789cc091c8df
Maryna <marynahaidashevska@gmail.com>
2019-07-30 16:21:47 -0500

    Maryna added footer part

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,61 @@
 
 
 </div><!--End main_content-->
+<!-- Footer -->
+<footer class="page-footer">
+	
+        <div class="row">
+            <div class="col-8">
+			    <div class="footer-logo"><span>Domino's</span><br />
+			  	<span class="nutrition">Nutrition</span></div>
+				  	<div class="footer-para"><p>
+				      We choose our ingredients on the basis of safety, taste, and nutritional<br />
+				      content to bring our consumers what they want. Dominos Pizza dedicates<br />
+				      its attention, energy, and resourses to one mission: deliver a delicious,<br />
+				      not and fresh pizza every time.
+				      </p>
+				  	</div><!-- footer-para ends -->
+				  </div><!-- col ends -->
+				  	<div class="row">
+			            <div class="col-auto">
+			            	<span class="footer-links"><a href="#">About us</a></span><br />
+			            	<span class="footer-links"><a href="#">Privacy</a></span><br />
+			            	<span class="footer-links"><a href="#">Term of use</a></span><br />
+			            </div><!-- col ends -->
+			        
+			            <div class="col-auto">
+			            	<span class="footer-links"><a href="#">Contact us</a></span><br />
+			            	<span class="footer-links"><a href="#">Nutrition guide</a></span><br />
+			            	<span class="footer-links"><a href="#">Gift cards</a></span><br />
+			            </div><!-- col ends -->
+		            </div><!-- row ends -->
+		        </div><!-- row ends -->
+				  	<!-- Copyright -->
+
+				  	<hr class="footer-line">
+				  	<div class="row">
+				  		
+				  	   <div class="col-sm-4">
+					       <div class="footer-copyright">©2019 Domino's IP Holder LLC. Domino's&reg;<br />
+						  	Domino's&reg; Pizza<br />
+						  	The modular logo are registered trademarks of<br />
+						  	Domino's&reg; IP Holder LLC
+							</div><!-- Copyright -->
+						</div>						  
+	                        
+                        <div class="col-sm-8"> 	
+					  	   	<img src="logo_footer.png" style="margin-left: 170px; width: 30%;" >
+	
+					  	   	<a href="https://www.facebook.com/dominoswinnipeg/"><img src="fb.png" style="margin-left: 70px; padding-left: 30px;" ></a>
+					  	   	<img src="twitter.png" style="padding-left: 30px;" >
+					  	   	<img src="instagram.png" style="padding-left: 30px;" >
+					  	   	
+					  	   </div>
+				  	        
+	                    </div><!-- row ends -->
+
+           
+               		   
+
+</footer>
+<!-- Footer -->
