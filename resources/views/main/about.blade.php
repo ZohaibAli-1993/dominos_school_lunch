7d2a78374f471e78c9b3675510eeb1b523cf9f62
Aman <adeepkaur13@outlook.com>
2019-07-30 15:45:55 -0500

    header link

--- resources/views/main/about.blade.php
+++ resources/views/main/about.blade.php
@@ -2,10 +2,35 @@
 
 @section('content')
 	<div class="container">
-		<div class="">
+		<div class="row">
 
 
-		</div>
-	</div>
+		</div><!-- end row -->
+
+		<div class="row">
+			<div class="col-sm-2" style="color:blue;">
+				<h2>About Us</h2>
+
+				<p>
+					Every pizza ordered has the potential to be a totally unique creation. Nutritional information is provided for each of the elements that go into a pizza: what size pizza, what type of crust, sause, toppings. When you create your own pizza, to see the total picture of what you are ordering, add together the numbers for each elements from these charts.
+				</p>
+
+				<p>
+					Lunch is an important meal of a day. Food is what gives you energy. Lunch raises your blood sugar level in the middle of the day, making you be able to focus for the rest of the afternoon. For kids, lunch is even more important becuase this is when they get their vitamins and nutrients for the day.
+				</p>
+
+				<p>
+					Domino's Pizza helps them grow, to be energetic during all activities by supplying meals for schools lunches. This program is also beneficial for the parents. They do not need to feel out the papers for ordering food, they can rest couple days per week without packing lunches for their little ones. Finally, these lunches makes school's lives easier, they do not need to collect money from all the kids, to count amount of slices and convert them to pizzas.
+					With Domino's delivery kids will get fresh pizza very quickly.
+				</p>
+
+			</div>
+
+			<div class="col-sm-2" style="color:#0f0;">
+
+			</div>
+
+		</div><!-- end row -->
+	</div><!-- end container -->
 
 @endsection
