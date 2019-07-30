c7426d923d38f30f9b706d09d20c6a00f229ecb2
root <root@localhost.localdomain>
2019-07-30 16:43:19 -0500

    success styles

--- resources/views/main/sucess.blade.php
+++ resources/views/main/sucess.blade.php
@@ -7,4 +7,22 @@
     <h3>Thanks for your message!</h3>
     <h4>We will be in touch soon.</h4>
 </div>
+
+
+<div>
+
+<input type="text" id="select_date" class="selecteShipDate" />
+
+</div>
+
+<script>
+$(document).ready(function () {
+    $input = $("#select_date");
+    $input.datepicker({
+        format: 'dd MM yyyy'
+    });
+    $input.data('datepicker').hide = function () {};
+    $input.datepicker('show');
+});
+</script>
 @endsection
