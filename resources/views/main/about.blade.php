8ebecc39c9335230945d5cab5c1c16e109136f05
Aman <adeepkaur13@outlook.com>
2019-07-30 16:56:11 -0500

    updated about page

--- resources/views/main/about.blade.php
+++ resources/views/main/about.blade.php
@@ -2,14 +2,18 @@
 
 @section('content')
 	<div class="container">
-		<div class="row">
-
+		<div class="row" id="hero-image">
+			<div class="col">
+				<img id="about-hero-image" src="img/pizza.jpg" alt="Pizza" />
+			</div>
 
 		</div><!-- end row -->
 
 		<div class="row">
-			<div class="col-sm-2" style="color:blue;">
+			<div class="col">
 				<h2>About Us</h2>
+
+				<img id="about-image" src="img/Pizza_500.png" alt="Pizza Girl" />
 
 				<p>
 					Every pizza ordered has the potential to be a totally unique creation. Nutritional information is provided for each of the elements that go into a pizza: what size pizza, what type of crust, sause, toppings. When you create your own pizza, to see the total picture of what you are ordering, add together the numbers for each elements from these charts.
@@ -21,12 +25,11 @@
 
 				<p>
 					Domino's Pizza helps them grow, to be energetic during all activities by supplying meals for schools lunches. This program is also beneficial for the parents. They do not need to feel out the papers for ordering food, they can rest couple days per week without packing lunches for their little ones. Finally, these lunches makes school's lives easier, they do not need to collect money from all the kids, to count amount of slices and convert them to pizzas.
-					With Domino's delivery kids will get fresh pizza very quickly.
 				</p>
 
-			</div>
-
-			<div class="col-sm-2" style="color:#0f0;">
+				<p>
+					With Domino's delivery kids will get fresh pizza very quickly.
+				</p>
 
 			</div>
 
