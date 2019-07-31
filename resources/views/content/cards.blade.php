6138de0db8d931c7776bbf979273e3065f2e3b82
Maryna <marynahaidashevska@gmail.com>
2019-07-31 17:51:28 -0500

    Gift cars page added

--- resources/views/content/cards.blade.php
+++ resources/views/content/cards.blade.php
@@ -6,6 +6,54 @@
 
 <h1>Gift Cards</h1>
 
+<h3 style="font-style: italic;">Domino's gift cards can be purchased at the following retail stores in Winnipeg</h3>
+
+<div class="row">
+	<div class="col-6">
+    <img src="/img/gift_card.jpg" width="100%">
+	</div>
+
+	<div class="col-6">
+    	<div class="row">
+    		<div class="col-4" class="gift-img" style="margin-bottom: 20px;">
+    			<img src="/img/walmart.png" width="100%">
+    		</div>
+    		<div class="col-4">
+    			<img src="/img/giant_tiger.png" width="100%">
+    		</div>
+    		<div class="col-4">
+    			<img src="/img/pharmasave.png" width="100%">
+    		</div>
+    	</div>
+    	<div class="row">
+    		<div class="col-4"  class="gift-img" style="margin-bottom: 20px;">
+    			<img src="/img/petro_canada.png" width="100%">
+    		</div>
+    		<div class="col-4">
+    			<img src="/img/husky.png" width="100%">
+    		</div>
+    		<div class="col-4">
+    			<img src="/img/shell.png" width="100%">
+    		</div>
+    	</div>
+
+    	<div class="row">
+    		<div class="col-4"  class="gift-img" style="margin-bottom: 20px;">
+    			<img src="/img/outfitters.png" width="100%">
+    		</div>
+    		<div class="col-4">
+    			<img src="/img/toys.png" width="100%">
+    		</div>
+    		<div class="col-4">
+    			<img src="/img/money_mart.png" width="100%">
+    		</div>
+    	</div>
+    </div>
+</div>
+
+
+</div>
+
 
 </div>
 @endsection
