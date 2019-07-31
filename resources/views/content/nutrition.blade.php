d3d91a1b3f72cd0a3772c98cb128940d3794ab74
Maryna <marynahaidashevska@gmail.com>
2019-07-31 15:44:01 -0500

    NUtrition guide page and images added

--- resources/views/content/nutrition.blade.php
+++ resources/views/content/nutrition.blade.php
@@ -3,6 +3,176 @@
 @section('content')
 
 
-<h1>Nutrition</h1>
+<h1>Nutrition Guide</h1>
+
+<h3>Using the Canadian Food Guide as a reference, Domino’s Pizza can be part of a healthy, balanced diet.</h3>
+<p>Because pizza is customizable, it is possible to enjoy a variety of foods (bread, vegetables, cheese and meats) on a single slice. We choose our ingredients on the basis of safety, taste, and nutritional content to bring our consumers what they want. Domino’s Pizza dedicates its attention, energy, and resources to one mission: deliver a delicious, hot and fresh pizza every time.</p>
+
+<div class="row">
+	<div class="col">
+		<img src="img/guides/guide_2.png" width="100%" class="img-fluid">
+    </div>
+</div>
+
+<div class="row">
+	<div class="col">
+		<img src="img/guides/guide_1.png" width="100%" class="img-fluid">
+    </div>
+</div>
+
+<div class="row">
+	<div class="col">
+		<img src="img/guides/guide_3.png" width="100%" class="img-fluid">
+    </div>
+</div>
+
+<div class="row">
+	<div class="col">
+		<img src="img/guides/guide_4.png" width="100%" class="img-fluid">
+    </div>
+</div>
+
+<div class="row">
+	<div class="col">
+		<img src="img/guides/guide_5.png" width="100%" class="img-fluid">
+    </div>
+</div>
+
+<div class="row">
+	<div class="col">
+		<img src="img/guides/guide_6.png" width="100%" class="img-fluid">
+    </div>
+</div>
+
+<div class="row">
+	<div class="col">
+		<img src="img/guides/guide_7.png" width="100%" class="img-fluid">
+	</div>
+</div>
+
+<div class="row">
+	<div class="col">
+		<img src="img/guides/guide_9.png" width="100%" class="img-fluid">
+	</div>
+</div>
+
+<div class="row">
+	<div class="col">
+		<img src="img/guides/guide_10.png" width="100%" class="img-fluid">
+	</div>
+</div>
+
+<div class="row">
+	<div class="col">
+		<img src="img/guides/guide_11.png" width="100%" class="img-fluid">
+	</div>
+</div>
+
+<div class="row">
+	<div class="col">
+		<img src="img/guides/guide_12.png" width="100%" class="img-fluid">
+	</div>
+</div>
+
+<div class="row">
+	<div class="col">
+		<img src="img/guides/guide_13.png" width="100%" class="img-fluid">
+    </div>
+</div>
+
+<div class="row">
+	<div class="col">
+		<img src="img/guides/guide_14.png" width="100%" class="img-fluid">
+    </div>
+</div>
+
+<p class="guide">Every pizza ordered has the potential to be a totally unique creation, and this nutritional guide reflects that range of possibilities. Nutritional information is provided for each of the elements that go into a pizza: what size pizza, what type of crust, sauce, toppings. When you create your own pizza, to see the total picture of what you are ordering, add together the numbers for each element from these charts. For Feast Pizzas and side items, there is no need to add; the ranges for these items are the total. *** Limited availability. Check with your local store</p>
+
+<div class="row">
+	<div class="col">
+		<img src="img/guides/guide_15.png" width="100%" class="img-fluid">
+    </div>
+</div>
+
+<div class="row">
+	<div class="col">
+		<img src="img/guides/guide_16.png" width="100%" class="img-fluid">
+    </div>
+</div>
+
+<div class="row">
+	<div class="col">
+		<img src="img/guides/guide_17.png" width="100%" class="img-fluid">
+    </div>
+</div>
+
+<div class="row">
+	<div class="col">
+		<img src="img/guides/guide_18.png" width="100%" class="img-fluid">
+    </div>
+</div>
+
+<div class="row">
+	<div class="col">
+		<img src="img/guides/guide_19.png" width="100%" class="img-fluid">
+    </div>
+</div>
+
+<div class="row">
+	<div class="col">
+		<img src="img/guides/guide_20.png" width="100%" class="img-fluid">
+    </div>
+</div>
+
+<div class="row">
+	<div class="col">
+		<img src="img/guides/guide_21.png" width="100%" class="img-fluid">
+    </div>
+</div>
+
+<div class="row">
+	<div class="col">
+		<img src="img/guides/guide_22.png" width="100%" class="img-fluid">
+    </div>
+</div>
+
+<div class="row">
+	<div class="col">
+		<img src="img/guides/guide_23.png" width="100%" class="img-fluid">
+    </div>
+</div>
+
+<div class="row">
+	<div class="col">
+		<img src="img/guides/guide_24.png" width="100%" class="img-fluid">
+    </div>
+</div>
+
+<div class="row">
+	<div class="col">
+		<img src="img/guides/guide_25.png" width="100%" class="img-fluid">
+    </div>
+</div>
+
+<div class="row">
+	<div class="col">
+		<img src="img/guides/guide_26.png" width="100%" class="img-fluid">
+    </div>
+</div>
+
+<div class="row">
+	<div class="col">
+		<img src="img/guides/guide_27.png" width="100%" class="img-fluid">
+    </div>
+</div>
+
+<div class="row">
+	<div class="col">
+		<img src="img/guides/guide_28.png" width="100%" class="img-fluid">
+    </div>
+</div>
+
+<p class="guide">The pizza products listed in this publication, when made with approved Domino’s Pizza ingredients, will provide the nutritional composition as indicated. Information may vary slightly depending on location and supplier. The availability of optional toppings may vary by store. The nutrition information is generated by the industry standard Genesis R&D Nutritional software. The in¬gredient listings are provided by ingredient manufacturers. Domino’s Pizza of Canada, Domino’s Pizza LLC, its franchisees and employees do not assume responsibility for a particular sensitivity or allergy to any food provided in our stores. This guide includes only standard menu items.</p>
+
 
 @endsection
