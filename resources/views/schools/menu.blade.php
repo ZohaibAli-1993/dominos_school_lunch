546e68e9692429596a20be1f615c1bf3582dcaea
Maryna <marynahaidashevska@gmail.com>
2019-07-31 14:15:35 -0500

    Terms, menu, policy pages added

---
+++ resources/views/schools/menu.blade.php
@@ -0,0 +1,120 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>Menu</h1>
+
+<h2>12'' pizza cut into 1/4</h2>
+
+
+
+<div class="row">
+
+	<div class="col">
+		<div class="menu-image"><img src="img/pizza_mix.jpg" width="100%" height="200">
+		<h3>Cheese Pizza</h3>
+			  <strong>$ 2.25/piece</strong><br />
+			  	Products Details<br />
+			  	Big Slice Cheese Pizza<br />
+			    <a href="#" type="button" id="menu-add-btn">Add</a></div>
+
+	</div><!-- col ends -->
+
+	<div class="col">
+		<div class="menu-image"><img src="img/pizza_hawaii.jpg" width="100%" height="200">
+		<h3>Ham & Pinnaple Pizza</h3>
+		<strong>$ 2.75/piece</strong><br />
+		Product Details:<br />
+		Ham & Pinnaple Pizza<br />
+		<a type="button" id="menu-add-btn">Add</a></div>
+
+
+	</div><!-- col ends -->
+
+	<div class="col">
+		<div class="menu-image"><img src="img/pizza_peperoni.jpg" width="100%" height="200">
+		<h3>Pepperoni Pizza</h3>
+		<strong>$ 2.25/piece</strong><br />
+		Product Details:<br />
+		Big Slice Pepperoni Pizza<br />
+		<a type="button" id="menu-add-btn">Add</a></div>
+
+	</div><!-- col ends -->
+
+	
+</div><!-- row ends -->
+
+
+<h2>14'' pizza cut into 1/8</h2>
+    
+<div class="row">
+	<div class="col">
+		<div class="menu-image"><img src="img/pizza_mix.jpg" width="100%" height="200">
+		<h3>Cheese Pizza</h3>
+			  <strong>$ 1.10/piece</strong><br />
+			  	Products Details<br />
+			  	Big Slice Cheese Pizza<br />
+			    <a type="button" id="menu-add-btn">Add</a></div>
+
+	</div><!-- col ends -->
+
+	<div class="col" class="menu-image">
+		<div class="menu-image"><img src="img/pizza_hawaii.jpg" width="100%" height="200">
+		<h3>Ham & Pinnaple Pizza</h3>
+		<strong>$ 1.20/piece</strong><br />
+		Product Details:<br />
+		Ham & Pinnaple Pizza<br />
+		<a type="button"  id="menu-add-btn">Add</a></div>
+
+	</div><!-- col ends -->
+
+	<div class="col" class="menu-image">
+		<div class="menu-image"><img src="img/pizza_peperoni.jpg" width="100%" height="200">
+		<h3>Pepperoni Pizza</h3>
+		<strong>$ 1.10/piece</strong><br />
+		Product Details:<br />
+		Big Slice Pepperoni Pizza<br />
+		<a type="button"  id="menu-add-btn">Add</a></div>
+
+	</div><!-- col ends -->
+
+
+</div><!-- row ends -->
+
+<h2>10'' pizza cut into 1/4 Gluten Free</h2>
+    
+<div class="row">
+	<div class="col">
+		<div class="menu-image"><img src="img/pizza_mix.jpg" width="100%" height="200">
+		<h3>Cheese Pizza</h3>
+			  <strong>$ 1.10/piece</strong><br />
+			  	Products Details<br />
+			  	Big Slice Cheese Pizza<br />
+			    <a type="button"  id="menu-add-btn">Add</a></div>
+
+	</div><!-- col ends -->
+
+	<div class="col">
+		<div class="menu-image"><img src="img/pizza_hawaii.jpg" width="100%" height="200">
+		<h3>Ham & Pinnaple Pizza</h3>
+		<strong>$ 1.20/piece</strong><br />
+		Product Details:<br />
+		Ham & Pinnaple Pizza<br />
+		<a type="button"  id="menu-add-btn">Add</a></div>
+
+	</div><!-- col ends -->
+
+	<div class="col">
+		<div class="menu-image"><img src="img/pizza_peperoni.jpg" width="100%" height="200">
+		<h3>Pepperoni Pizza</h3>
+		<strong>$ 1.10/piece</strong><br />
+		Product Details:<br />
+		Big Slice Pepperoni Pizza<br />
+		<a type="button"  id="menu-add-btn">Add</a></p></div>
+
+	</div><!-- col ends -->
+
+
+</div><!-- row ends -->
+
+@endsection
