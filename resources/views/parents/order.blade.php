207efc51d6956e5e334467acbc701c26fa3f05f4
root <root@localhost.localdomain>
2019-08-07 15:01:42 -0500

    loop added order

--- resources/views/parents/order.blade.php
+++ resources/views/parents/order.blade.php
@@ -12,72 +12,25 @@
         <div class="col-xs-12 main-tab">
             <nav>
                 <div class="nav nav-tabs nav-fill" id="nav-tab" role="tablist">
+                <?php $i=0;?>
+                @foreach($students as $student)
+                    <?php $i++; ?>
                     <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab"
-                        aria-controls="nav-home" aria-selected="true">Student 1</a>
-                    <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab"
-                        aria-controls="nav-profile" aria-selected="false">Student 2</a>
-                    <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab"
-                        aria-controls="nav-contact" aria-selected="false">Student 3</a>
-                    <a class="nav-item nav-link" id="nav-about-tab" data-toggle="tab" href="#nav-about" role="tab"
-                        aria-controls="nav-about" aria-selected="false">Student 4</a>
+                        aria-controls="nav-home" aria-selected="true">Student - {{ $i }}</a>
+                @endforeach
                 </div>
             </nav>
             <div class="tab-content py-3 px-3 px-sm-0" id="nav-tabContent">
-                <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-                    <strong>School:</strong> <br />
-                    <strong>Classroom:</strong>
-                    <div class="table-responsive text-nowrap">
+                <?php $i=0; reset($students);  ?>    
+                
+                @foreach($students as $student)
+                    <?php $i++; ?>
+                <div class="tab-pane fade" id="nav-profile <?= $i ?>" role="tabpanel" aria-labelledby="nav-profile-tab">
+                    Student <?= $i ?>
+                </div>
+                @endforeach
 
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Event Date</th>
-                                    <th scope="col">Status</th>
-                                    <th scope="col">Total Amount</th>
-                                    <th scope="col">Order</th>
-                                    <th scope="col">Invoice</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <th scope="row">1</th>
-                                    <td>08/20/2019</td>
-                                    <td>Paid</td>
-                                    <td>$ 20.00</td>
-                                    <td>001</td>
-                                    <td><a href="#">Download</a></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">2</th>
-                                    <td>08/20/2019</td>
-                                    <td>Paid</td>
-                                    <td>$ 20.00</td>
-                                    <td>002</td>
-                                    <td><a href="#">Download</a></td>
-                                </tr>
-                                <tr>
-                                    <th scope="row">3</th>
-                                    <td>08/20/2019</td>
-                                    <td>Paid</td>
-                                    <td>$ 20.00</td>
-                                    <td>003</td>
-                                    <td><a href="#">Download</a></td>
-                                </tr>
-                            </tbody>
-                        </table>
 
-                    </div>
-                </div>
-                <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
-                    Student 2 Table
-                </div>
-                <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
-                    Student 3 Table
-                </div>
-                <div class="tab-pane fade" id="nav-about" role="tabpanel" aria-labelledby="nav-about-tab">
-                    Student 4 Table
-                </div>
             </div>
             <div class="alignleft">
             <a class="btn btn-danger " href="">Add New</a>
