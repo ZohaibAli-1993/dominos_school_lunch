123fa9ee718794a50b3fd0f75fea1ce5f51a08d3
Alessandra <alesericolla@hotmail.com>
2019-08-09 14:23:50 -0500

    Corrected conflict errors

--- routes/web.php
+++ routes/web.php
@@ -237,18 +237,6 @@ Route::post('/parents/{parentRegister}/student/add', 'Students\StudentsControlle
 Route::get('/home', 'Dominos\SubscriptionsController@store')->name('home');
 Route::post('/home', 'Dominos\SubscriptionsController@store');
 
-/*
-|--------------------------------------------------------------------------
-| SCHOOL EVENTS ROUTES
-|--------------------------------------------------------------------------
-*/
-Route::get('/schools/events', 'Schools\EventsController@index');
-
-Route::get('/schools/events/edit/{event}', 'Schools\EventsController@edit');
-
-Route::put('/schools/events', 'Schools\EventsController@update');
-
-Route::get('/schools/events','Schools\EventsController@index');
 
 
 Route::get('/parents/order','Students\OrdersController@showOrder');
@@ -268,20 +256,6 @@ Route::PUT('/parents/{parentRegister}/{student}/edit', 'Students\StudentsControl
 Route::get('/parents/{parentRegister}/{token}/student/add', 'Students\StudentsController@create');
 
 Route::post('/parents/{parentRegister}/{token}/student/add', 'Students\StudentsController@store');
-
-
-/*
-|--------------------------------------------------------------------------
-| SCHOOL EVENTS ROUTES
-|--------------------------------------------------------------------------
-*/
-Route::get('/schools/events', 'Schools\EventsController@index');
-
-Route::get('/schools/events/edit/{event}', 'Schools\EventsController@edit');
-
-Route::put('/schools/events', 'Schools\EventsController@update');
-
-Route::get('/schools/events','Schools\EventsController@index');
 
 
 /**FOOTER CONTENT LINKS */
