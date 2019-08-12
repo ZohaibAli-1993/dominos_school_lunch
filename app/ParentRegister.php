6b1ffc0ca2fa515d83db73e9ad610d8c18c89250
DaphneNguyen <truong-n51@webmail.uwinnipeg.ca>
2019-08-12 10:31:54 -0500

    add migration

---
+++ app/ParentRegister.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ParentRegister extends Model
+{
+    /**
+     * Defines primary key associated with the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'idparent';
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+
+
+    protected $table = 'parents'; 
+ 
+    protected $fillable=['first_name','last_name','email','phone','idparent','password','captcha'];
+
+}
---
+++ database/migrations/2019_08_06_232300_create_parents_register_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateParentsRegisterTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('parents', function (Blueprint $table) {
+            $table->bigIncrements('idparent');
+            $table->string('first_name',30); 
+            $table->string('last_name',30); 
+            $table->string('email',255); 
+            $table->string('phone',15); 
+            $table->string('password',255); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::dropIfExists('parents');
+
+    }
+}
